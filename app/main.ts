1d880cd30518b8ea0af6747c905d911692f0951e
amajid09 <amajid0981@gmail.com>
2025-01-19 14:40:06 +0200

    add an exit code

--- app/main.ts
+++ app/main.ts
@@ -4,12 +4,23 @@ const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
+let run = true;
+let invalid = true;
 function runCommand() {
   rl.question("$ ", (answer) => {
-    const [command, code] = answer.split(" ");
-    if (command !== "exit" && code !== "0") {
+    if (
+      answer.split(" ").length > 1 &&
+      answer.split(" ")[0] === "exit" &&
+      answer.split(" ")[1] === "0"
+    ) {
+      run = false;
+      invalid = false;
+      rl.close()
+    }
+    if (invalid) {
       rl.write(`${answer}: command not found\n`);
+    }
+    if (run) {
       runCommand();
     }
   });
